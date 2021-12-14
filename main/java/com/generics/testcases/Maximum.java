4fbd5bb8e907903173f02c429074bd82a4bc0ff9
Pratiklahamge <plahamge2@gmail.com>
2021-12-14 13:54:27 +0530

    Added: program to find maximum of String Char

---
+++ main/java/com/generics/testcases/Maximum.java
@@ -0,0 +1,13 @@
+package com.generics.testcases;
+
+public class Maximum {
+	   public static String getmaximum(String value1, String value2, String value3) {
+	        String max = value1;
+	        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+	            max = value2;
+	        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+	            max = value3;
+	        System.out.println(max);
+	        return max;
+	    }
+}
---
+++ test/java/com/bridgelabz/Generics/MaximumTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.Generics;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.generics.testcases.Maximum;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class MaximumTest {
+	  @Test
+	    public void stringMax1() {
+	        String actualValue = Maximum.getmaximum("Aa","Bb","Cc");
+	        Assert.assertEquals("Cc",actualValue);
+	    }
+
+	    @Test
+	    public void stringMax2() {
+	        String actualValue = Maximum.getmaximum("abc","ABC","def");
+	        Assert.assertEquals("def", actualValue);
+	    }
+
+}
