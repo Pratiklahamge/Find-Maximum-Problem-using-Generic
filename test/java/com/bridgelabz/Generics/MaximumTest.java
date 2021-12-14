f2b61e42638ddc9e5ec88f9f16fe86a58dbbf154
Pratiklahamge <plahamge2@gmail.com>
2021-12-14 14:08:43 +0530

    Added: program to find Extend the max method

--- test/java/com/bridgelabz/Generics/MaximumTest.java
+++ test/java/com/bridgelabz/Generics/MaximumTest.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.Generics;
+import static org.junit.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,18 +8,22 @@ import com.generics.testcases.Maximum;
 /**
  * Unit test for simple App.
  */
-
 public class MaximumTest {
-	  @Test
-	    public void stringMax1() {
-	        String actualValue = Maximum.getmaximum("Aa","Bb","Cc");
-	        Assert.assertEquals("Cc",actualValue);
-	    }
+    @Test
+    public void intMax() {
+        int actualValue = Maximum.testMaximum(10, 20, 5);
+        Assert.assertEquals(20, actualValue);
+    }
 
-	    @Test
-	    public void stringMax2() {
-	        String actualValue = Maximum.getmaximum("abc","ABC","def");
-	        Assert.assertEquals("def", actualValue);
-	    }
+    @Test
+    public void floatMax() {
+        float actualValue = Maximum.testMaximum(11f,52f,36f);
+        Assert.assertEquals(52f,actualValue,0);
+    }
 
+    @Test
+    public void stringMax() {
+        String actualValue = Maximum.testMaximum("ABC","def","xyz");
+        Assert.assertEquals("xyz",actualValue);
+    }
 }
