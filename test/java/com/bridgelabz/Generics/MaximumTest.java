8252f63000c2dd8bec597679ee7110a49d8269b6
Pratik Lahamge <89896925+Pratiklahamge@users.noreply.github.com>
2021-12-14 12:37:53 +0530

    Added: program to find maximum of float number

---
+++ test/java/com/bridgelabz/Generics/MaximumTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.Generics;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.generics.testcases.Maximum;
+
+
+public class MaximumTest {
+    @Test
+    public void floatMax1() {
+        float actualValue = Maximum.getmaximum(12f,14f,15f);
+        Assert.assertEquals(15,actualValue ,0);
+    }
+
+    @Test
+    public void floatMax2() {
+        float actualValue = Maximum.getmaximum(1000f,545f,746f);
+        Assert.assertEquals(1000,actualValue, 0);
+    }
+
+}
